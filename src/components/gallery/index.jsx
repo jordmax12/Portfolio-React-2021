08fd1c06d1fb22e45d92e97ffd97f51df3bc5b6f
Jordan <jordmax12@gmail.com>
2020-12-25 13:24:37 -0600

    implemented nav to gallery, although im not happy with the result.. need to make this better

--- src/components/gallery/index.jsx
+++ src/components/gallery/index.jsx
@@ -1,27 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import RatingsCard from "../ratings-card";
-import { useSpring, animated,  useTransition, Spring } from "react-spring";
-// import bezier from 'bezier-easing'
+import { animated,  useTransition } from "react-spring";
 import "./gallery.css";
-
-const get_number_inbetween = (min, max) => {
-  return Math.floor(Math.random() * max) + (min)
-}
 
 export const Gallery = (props) => {
   const { projects } = props;
-  console.log('logging projects', projects)
-  const [cards, setCards] = useState(projects);
-  console.log('logging cards????', cards);
-  // const toggle_divs = () => {
-  //   let test = [];
-  //   setCards(
-  //     test = projects.filter((x) => x.title == "enogen" || x.title == "agriedge")
-  //   );
-  //   setTimeout(() => {
-  //     setCards(test);
-  //   }, 800);
-  // };
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    setCards(props.projects)
+  }, [props.projects])
 
   const transitions = useTransition(cards, card => card.key, {
     from: { transform: "translate3d(0,-4000px,0)", opacity: 0, display: 'inline-block', padding: '10px' },
@@ -34,16 +22,12 @@ export const Gallery = (props) => {
 
   return (
     <div style={{maxWidth: '830px', marginRight: 'auto', marginLeft: 'auto'}}>
-      <div style={{width: '100%'}}>
-        {/* <button onClick={toggle_divs}>SHOW/HIDE</button> */}
-      </div>
         {transitions.map(({ item, key, props }) =>
             item && <animated.div key={key} style={props}>
               <RatingsCard key={key} {...item}  />
             </animated.div>
           )}
     </div>
-    
   );
 };
 
