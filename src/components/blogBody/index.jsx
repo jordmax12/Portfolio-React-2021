7a75d5fc2bf00984608b7923e4541c245888be9f
Jordan <jordmax12@gmail.com>
2020-12-27 09:37:24 -0600

    blog progress

--- src/components/blogBody/index.jsx
+++ src/components/blogBody/index.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import './blogBody.module.scss'
 
 const BlogBody = (props) => {
-  const { children } = props;
+  const { children, headline } = props;
   return (
-    <div> {children} </div>
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+            {headline}
+        </div>
+        <div className="col-12">
+            {children}
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/blogPost/index.jsx
+++ src/components/blogPost/index.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import './blogPost.module.scss'
 import BlogBody from '../blogBody'
+import Div from '../div'
 
 const BlogPost = (props) => {
-  const { children } = props;
   return (
-    <>
-      {children}
-      <BlogBody>
-        Inside Blog Body
-      </BlogBody>
-    </>
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <BlogBody headline="This is a test blog post">
+            Inside Blog Body
+          </BlogBody>
+        </div>
+      </div>
+    </div>
   )
 }
 
