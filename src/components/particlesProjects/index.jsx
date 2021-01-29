76c8751d30b383b4e7d8143f1e5994f01f7a1f27
Jordan <jordmax12@gmail.com>
2021-01-29 09:49:09 -0600

    begin work on projects background

--- src/components/particlesProjects/index.jsx
+++ src/components/particlesProjects/index.jsx
@@ -8,7 +8,139 @@ const ParticlesProjects = (props) => {
     <div className={styles.particlesLandingContainer}>
       <div className={styles.particlesLandingHolder}>
         <Particles
-          height={"100vh"}
+          height={"250px"}
+          params={{
+            particles: {
+              number: {
+                value: 200,
+                limit: 300,
+                density: {
+                  enable: true,
+                  value_area: 800
+                }
+              },
+              color: {
+                value: "#ffffff"
+              },
+              shape: {
+                type: "circle",
+                stroke: {
+                  width: 0,
+                  color: "#000000"
+                },
+                polygon: {
+                  nb_sides: 5
+                },
+                image: {
+                  src: "images/github.svg",
+                  width: 100,
+                  height: 100
+                }
+              },
+              opacity: {
+                value: 0.5,
+                random: true,
+                anim: {
+                  enable: true,
+                  speed: 1,
+                  opacity_min: 0.5,
+                  sync: false
+                }
+              },
+              size: {
+                value: 30,
+                random: true,
+                anim: {
+                  enable: true,
+                  speed: 10,
+                  size_min: 10,
+                  sync: false
+                }
+              },
+              line_linked: {
+                enable: false,
+                distance: 100,
+                color: "#ffffff",
+                opacity: 1,
+                width: 1
+              },
+              move: {
+                enable: true,
+                speed: 3,
+                direction: "none",
+                random: false,
+                straight: false,
+                out_mode: "out",
+                bounce: false,
+                attract: {
+                  enable: false,
+                  rotateX: 600,
+                  rotateY: 1200
+                }
+              }
+            },
+            interactivity: {
+              detect_on: "canvas",
+              events: {
+                onHover: {
+                  enable: true,
+                  mode: "bubble",
+                  parallax: {
+                    enable: true,
+                    force: 60,
+                    smooth: 10
+                  }
+                },
+                onClick: {
+                  enable: true,
+                  mode: "push"
+                },
+                resize: true
+              },
+              modes: {
+                grab: {
+                  distance: 400,
+                  lineLinked: {
+                    opacity: 1
+                  }
+                },
+                bubble: {
+                  distance: 400,
+                  size: 100,
+                  duration: 2,
+                  opacity: 1,
+                  speed: 2
+                },
+                repulse: {
+                  distance: 200
+                },
+                push: {
+                  particles_nb: 4
+                },
+                remove: {
+                  particles_nb: 2
+                }
+              }
+            },
+            backgroundMask: {
+              enable: true,
+              cover: {
+                color: {
+                  value: {
+                    r: 34,
+                    g: 40,
+                    b: 49
+                  }
+                }
+              }
+            },
+            retina_detect: true,
+            fps_limit: 30,
+            background: {
+              image:
+                "url('https://cdn.matteobruni.it/images/particles/background3.jpg')"
+            }
+          }}
         //   params={{
         //     particles: {
         //       number: {
@@ -94,70 +226,6 @@ const ParticlesProjects = (props) => {
         //     },
         //     retina_detect: true,
         //   }}
-        params={{
-            fps_limit: 28,
-            particles: {
-                collisions: {
-                    enable: false
-                },
-                number: {
-                    value: 200,
-                    density: {
-                        enable: false
-                    }
-                },
-                line_linked: {
-                    enable: true,
-                    distance: 30,
-                    opacity: 0.4
-                },
-                move: {
-                    speed: 1
-                },
-                opacity: {
-                    anim: {
-                        enable: true,
-                        opacity_min: 0.05,
-                        speed: 1,
-                        sync: false
-                    },
-                    value: 0.4
-                }
-            },
-            polygon: {
-                enable: true,
-                scale: 0.5,
-                type: "inline",
-                move: {
-                    radius: 10
-                },
-                url: "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
-                inline: {
-                    arrangement: "equidistant"
-                },
-                draw: {
-                    enable: true,
-                    stroke: {
-                        color: "rgba(255, 255, 255, .2)"
-                    }
-                }
-            },
-            retina_detect: false,
-            interactivity: {
-                events: {
-                    onhover: {
-                        enable: true,
-                        mode: "bubble"
-                    }
-                },
-                modes: {
-                    bubble: {
-                        size: 6,
-                        distance: 40
-                    }
-                }
-            }
-        }}
         />
       </div>
       {props.children}
