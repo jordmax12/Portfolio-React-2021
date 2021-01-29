f9293f518af62c284f5da506dde77c11448ef07a
Jordan <jordmax12@gmail.com>
2021-01-28 22:09:19 -0600

    save

--- src/views/Projects/index.jsx
+++ src/views/Projects/index.jsx
@@ -14,7 +14,6 @@ const Projects = () => {
   const [matchedProjects, setMatchedProjects] = useState(projects)
   const [changeProjects, setChangeProducts] = useState(false);
   const [show, setShow] = useState(false);
-  const [triggerBg, setTriggerBg] = useState(false);
   // TODO: is the following needed? review.
   /* eslint-disable no-unused-vars */
   const [isFirstAnimation, setIsFirstAnimation] = useState(false)
@@ -22,21 +21,17 @@ const Projects = () => {
   const [imageAlignment, setImageAlignment] = useState(random(0, 3))
   /* eslint-disable no-unused-vars */
   const [imagePosition, setImagePosition] = useState(getImagePosition(selectedTechId, imageAlignment))
-  /* eslint-disable no-unused-vars */
   const [backgroundTransition, setBackgroundTransition] = useState(getBackgroundTransition(
     selectedTechId,
     imageAlignment,
     isFirstAnimation
   ))
+  /* eslint-disable no-unused-vars */
   const [techTransitionAnimation, setTechTransitionAnimation] = useState({
     nodejs: {
       ...backgroundTransition,
       imagePosition
     }
-  })
-
-  useEffect(() => {
-    setTriggerBg(true)
   })
 
   useEffect(() => {
@@ -99,83 +94,82 @@ const Projects = () => {
         value => {
           return (
             <div style={{ width: '100%', height: '100%' }}>
-      <Div row fillParent align="stretch" className={styles.timeline_container}>
-        
-        {/* <img alt="tech-logo" src={bgTest} className={styles.background_static_image} /> */}
-        <div className="container">
-          <div className="row">
-            <div className="col-2">
-              <TimelineSelector
-                selectedId={selectedTechId}
-                listValue={technologies}
-                tech={tech}
-                onItemSelected={onTechSelected}
-              />
-            </div>
-            <div className="col-10">
-              <div className="gallery_container">
-                <Transition
-                  items={tech}
-                  keys={tech => tech.id}
-                  from={{ opacity: 0 }}
-                  enter={{ opacity: 1 }}
-                  leave={{ opacity: 0 }}
-                >
-                  {tech => tech.id && (
-                    value => {
-                      const { imagePosition, from, enter, leave } = techTransitionAnimation[tech.id];
-                      const fromAnimation = tech.id === selectedTechId ? from : enter;
-                      const toAnimation = tech.id === selectedTechId ? enter : leave;
-                      const isReactRelated = tech.id === "react"
-                      return (
-                        <Spring
-                          from={{
-                            opacity: isReactRelated ? fromAnimation.opacity : 1,
-                            transform: fromAnimation.transform,
-                          }}
-                          to={{
-                            opacity: isReactRelated ? toAnimation.opacity : 1,
-                            transform: toAnimation.transform,
-                          }}
+              <Div row fillParent align="stretch" className={styles.timeline_container}>
+                
+                {/* <img alt="tech-logo" src={bgTest} className={styles.background_static_image} /> */}
+                <div className="container">
+                  <div className="row">
+                    <div className="col-2">
+                      <TimelineSelector
+                        selectedId={selectedTechId}
+                        listValue={technologies}
+                        tech={tech}
+                        onItemSelected={onTechSelected}
+                      />
+                    </div>
+                    <div className="col-10">
+                      <div className="gallery_container">
+                        <Transition
+                          items={tech}
+                          keys={tech => tech.id}
+                          from={{ opacity: 0 }}
+                          enter={{ opacity: 1 }}
+                          leave={{ opacity: 0 }}
                         >
-                          {
-                            props => (
-                              <Div
-                                style={{
-                                  opacity: isReactRelated ? props.opacity : 1,
-                                  transform: !isReactRelated ? props.transform : "unset"
-                                }}
-                                className={styles.background_image_container}
-                              >
-                                <img
-                                  alt="background"
-                                  src={tech.backgroundImage}
-                                  style={{
-                                    left: 0,
-                                    right: imagePosition.right,
-                                    top: 0,
-                                    bottom: imagePosition.bottom,
-                                    transform: props.transform
+                          {tech => tech.id && (
+                            value => {
+                              const { imagePosition, from, enter, leave } = techTransitionAnimation[tech.id];
+                              const fromAnimation = tech.id === selectedTechId ? from : enter;
+                              const toAnimation = tech.id === selectedTechId ? enter : leave;
+                              const isReactRelated = tech.id === "react"
+                              return (
+                                <Spring
+                                  from={{
+                                    opacity: isReactRelated ? fromAnimation.opacity : 1,
+                                    transform: fromAnimation.transform,
                                   }}
-                                  className={styles.background_image}
-                                ></img>
-                              </Div>
-                            )
-                          }
-                        </Spring>
-                      )
-                    }
-                  )}
-                </Transition>
-              </div>
-              <Gallery
-                projects={matchedProjects} />
+                                  to={{
+                                    opacity: isReactRelated ? toAnimation.opacity : 1,
+                                    transform: toAnimation.transform,
+                                  }}
+                                >
+                                  {
+                                    props => (
+                                      <Div
+                                        style={{
+                                          opacity: isReactRelated ? props.opacity : 1,
+                                          transform: !isReactRelated ? props.transform : "unset"
+                                        }}
+                                        className={styles.background_image_container}
+                                      >
+                                        <img
+                                          alt="background"
+                                          src={tech.backgroundImage}
+                                          style={{
+                                            left: 0,
+                                            right: imagePosition.right,
+                                            top: 0,
+                                            bottom: imagePosition.bottom,
+                                            transform: props.transform
+                                          }}
+                                          className={styles.background_image}
+                                        ></img>
+                                      </Div>
+                                    )
+                                  }
+                                </Spring>
+                              )
+                            }
+                          )}
+                        </Transition>
+                      </div>
+                      <Gallery
+                        projects={matchedProjects} />
+                    </div>
+                  </div>
+                </div>
+              </Div>
             </div>
-          </div>
-        </div>
-        <BackgroundAnimator triggerBg={triggerBg} />
-      </Div>
-    </div>
           )
         }
       )
